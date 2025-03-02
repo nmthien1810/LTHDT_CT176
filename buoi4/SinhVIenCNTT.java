69720a0f7727a3c4592e6234bca89053251af62b
nmthien1810 <nmthien1810@gmail.com>
2025-03-02 15:27:21 +0700

    .

--- buoi4/SinhVIenCNTT.java
+++ buoi4/SinhVIenCNTT.java
@@ -1,5 +1,31 @@
 package buoi4;
 
-public class SinhVIenCNTT {
+import java.util.Scanner;
+
+import buoi3.SinhVien;
+
+public class SinhVienCNTT extends SinhVien {
+    private String taikhoan, matkhau, email;
     
+    public SinhVienCNTT() {
+        super();
+        taikhoan = new String();
+        matkhau = new String();
+        email = new String();
+    }
+
+    public SinhVienCNTT(SinhVienCNTT s) {
+        super(s);
+        taikhoan = new String(s.taikhoan);
+        matkhau = new String(s.matkhau);
+        email = new String(s.email);
+    }
+
+    public void nhapThongTin() {
+        super.nhapThongTin();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap tai khoan: ");
+        taikhoan = sc.nextLine();
+        
+    }
 }
