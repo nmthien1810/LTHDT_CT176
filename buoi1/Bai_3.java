bd1a0d072294794381856ad4fb5d6a5ba6d2f313
nmthien1810 <nmthien1810@gmail.com>
2025-03-15 16:48:32 +0700

    .

--- buoi1/Bai_3.java
+++ buoi1/Bai_3.java
@@ -4,15 +4,13 @@ import java.util.Scanner;
 
 public class Bai_3 {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            char ch;
-            do {
-                ch = sc.nextLine().charAt(0);
-                if (ch != 'q') {
-                    System.out.println(ch);
-                }
+        Scanner sc = new Scanner(System.in);
+        char ch;
+        do {
+            ch = sc.nextLine().charAt(0);
+            if (ch != 'q') {
+                System.out.println(ch);
             }
-            while (ch != 'q');
-        }
+        } while (ch != 'q');
     }
 }
